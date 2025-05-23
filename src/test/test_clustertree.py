d3081ef41326f86c7e54dc64936ceafcfdec34a6
swagle8987 <17229623+swagle8987@users.noreply.github.com>
2025-05-22 21:40:44 -0500

    Added tests for cluster_support

--- src/test/test_clustertree.py
+++ src/test/test_clustertree.py
@@ -17,7 +17,7 @@ class TestClusterComputation:
         assert dist == 2
     
     def test_cluster_affinity_tau(self):
-        return
+        return 
         ntax = 100
         taxon_ns = TaxonNamespace(["l{}".format(i) for i in range(ntax)])
         for i in range(1000):
@@ -39,3 +39,23 @@ class TestClusterComputation:
         dist = cluster_affinity.unrooted_cluster_affinity(self.t1,self.t2)
         assert dist == 1
 
+    def test_cluster_support_zero(self):
+        dist = cluster_affinity.rooted_cluster_support(self.t1,self.t1)
+        assert dist == 0
+
+    def test_cluster_support(self):
+        dist = cluster_affinity.rooted_cluster_support(self.t1,self.t2)
+        assert dist == 1
+
+    def test_cluster_support_phi(self):
+        ntax = 100
+        taxon_ns = TaxonNamespace(["l{}".format(i) for i in range(ntax)])
+        for i in range(1000):
+            t1 = treesim.birth_death_tree(birth_rate=1.0,death_rate=0,num_extant_tips=len(taxon_ns),taxon_namespace=taxon_ns)
+            t2 = treesim.birth_death_tree(birth_rate=1.0,death_rate=0,num_extant_tips=len(taxon_ns),taxon_namespace=taxon_ns)
+            dist = cluster_affinity.rooted_cluster_support(t1,t2)
+            phi = cluster_affinity.calculate_rooted_phi(t1)
+            assert dist >= 0,"{} {} {} {}".format(t1.as_string(schema="newick"),
+                                               t2.as_string(schema="newick"), 
+                                               cluster_affinity.rooted_cluster_affinity(t1,t2))
+            assert dist <= phi
