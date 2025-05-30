6a40517aa38bb28f1c18ed48a204d90cf842b077
swagle8987 <17229623+swagle8987@users.noreply.github.com>
2025-05-30 13:57:59 -0500

    Converted existing cluster affinity functions to ete4
    Signed-off-by: swagle8987 <17229623+swagle8987@users.noreply.github.com>

--- src/cluster_computation/cluster_affinity.py
+++ src/cluster_computation/cluster_affinity.py
@@ -10,20 +10,20 @@ config_handler.set_global(bar="bubbles",spinner="radioactive",file=stderr)
     cluster_affinity: Tree -> Tree -> int
 '''
 def rooted_cluster_affinity(t1,t2):
-    t1_cmap = convert_tree_to_cmap(t1)
+    t1_cmap = t1.get_cached_content(prop="name")
     tree_dist = 0
     with alive_bar(len(t1_cmap)) as bar:
-        for i in t1_cmap.values():
-            tree_dist += cluster_to_tree_affinity(i,t2)
+        for i in t1.traverse("postorder"):
+            tree_dist += cluster_to_tree_affinity(t1_cmap[i],t2)
             bar()
     return tree_dist
 
 def rooted_cluster_support(t1,t2):
-    t1_cmap = convert_tree_to_cmap(t1)
+    t1_cmap = t1.get_cached_content(prop="name")
     tree_dist = 0
     with alive_bar(len(t1_cmap)) as bar:
-        for i in t1_cmap.values():
-            tree_dist += cluster_to_tree_affinity(i,t2)/len(i)
+        for i in t1.traverse("postorder"):
+            tree_dist += cluster_to_tree_affinity(t1_cmap[i],t2)/len(t1_cmap[i])
             bar()
     return tree_dist
 
@@ -76,54 +76,37 @@ def unrooted_cdist(c,t2,n):
 def cluster_to_tree_affinity(c,t2):
     mindist = math.inf
     intersection_lookup = dict()
-    size_lookup = dict()
-    for i in t2.postorder_node_iter():
+    t2lookup = t2.get_cached_content(prop="name")
+    for i in t2.traverse("postorder"):
         intersection = 0
-        size = 0
-        if i.is_leaf():
-            size = 1
-            if i.taxon.label in c:
+        if i.is_leaf:
+            if i.name in c:
                 intersection = 1
             else:
                 intersection = 0
-                size_lookup[i] = 1
         else:
-            for ch in i.child_node_iter():
-                intersection += intersection_lookup[ch]
-                size += size_lookup[ch]
-        intersection_lookup[i] = intersection
-        size_lookup[i] = size
-        newdist = len(c) + size - 2*intersection
+            for ch in i.children:
+                intersection += intersection_lookup[ch.id]
+        intersection_lookup[i.id] = intersection
+        newdist = len(c) + len(t2lookup[i]) - 2*intersection
         if mindist > newdist:
-            mindist = newdist
+            mindist=newdist
     return mindist
-
-def convert_tree_to_cmap(t):
-    cluster_map = dict()
-    for i in t.postorder_node_iter():
-        if i.is_leaf():
-            c = {i.taxon.label}
-        else:
-            c = set()
-            for ch in i.child_node_iter():
-                c = c | cluster_map[ch]
-        cluster_map[i] = c
-    return cluster_map
 
 def calculate_rooted_tau(t):
     tau = 0
-    n = len(t.leaf_nodes())
+    n = len(t)
     sizemap = dict()
-    for i in t.postorder_node_iter():
-        if i.is_leaf():
+    for i in t.traverse("postorder"):
+        if i.is_leaf:
             s = 1
         else:
             s = 0
-            for ch in i.child_node_iter():
+            for ch in i.children:
                 s += sizemap[ch]
         sizemap[i] = s
         tau += min(s-1,n-s)
-    return tau
+    return tau 
 
 def calculate_harmonic_number(i):
     gamma = 0.57721566490153286060651209008240243
@@ -131,14 +114,14 @@ def calculate_harmonic_number(i):
 
 def calculate_rooted_phi(t):
     phi = 0
-    n = len(t.leaf_nodes())
+    n = len(t)
     sizemap = dict()
-    for i in t.postorder_node_iter():
-        if i.is_leaf():
+    for i in t.traverse("postorder"):
+        if i.is_leaf:
             s = 1
         else:
             s = 0
-            for ch in i.child_node_iter():
+            for ch in i.children:
                 s += sizemap[ch]
         sizemap[i] = s
         phi += min(s-1,n-s)/s
