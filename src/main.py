ae0680489be0177541cd9e40d474ec8ba5f9a00b
Sanket Wagle <swagle@iastate.edu>
2025-06-12 12:31:42 -0500

    Actually removed cluster_matrix this time

--- src/main.py
+++ src/main.py
@@ -9,35 +9,6 @@ from .utils import peek_line,convert_dict_to_2d_array, make_matrix_image, check_
 import os
 
 
-
-def cluster_matrix_script():
-    parser = argparse.ArgumentParser(
-            prog='Cluster Matrix',
-            description='Calculates the Asymmetric Cluster Affinity matrix for a list of trees',
-    )
-
-    parser.add_argument('t', help='The treefile containing a list of trees')
-    parser.add_argument('--filetype', help="The input file format")
-    parser.add_argument('--outfile', help="The output file",default="matrix.png")
-
-    args = parser.parse_args()
-
-    ftype = args.filetype
-    if not ftype:
-        ftype = "nexus" if peek_line(args.t) == "#NEXUS" else "newick"
-
-    tlist = dendropy.TreeList.get(path=args.t,schema=ftype,rooting="default-rooted")
-    if check_input_trees(tlist):
-        matrix_encoding = dict()
-        for indx,i in enumerate(tlist):
-            matrix_encoding[indx] = dict()
-            tau = calculate_rooted_tau(i)
-            for jndx,j in enumerate(tlist):
-                matrix_encoding[indx][jndx] = rooted_cluster_affinity(i,j)/tau
-        matrix = convert_dict_to_2d_array(matrix_encoding)
-        make_matrix_image(matrix,output_path=args.outfile)
-        print(matrix)
-
 def cluster_affinity_script():
 
     parser = argparse.ArgumentParser(
