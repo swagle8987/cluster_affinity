e48733ab814da93e65572fbbb0b139aa3f6d7465
Sanket Wagle <swagle@iastate.edu>
2025-07-11 19:02:47 -0500

    Added cli test and matrix.py

--- src/cluster_affinity/utils.py
+++ src/cluster_affinity/utils.py
@@ -21,27 +21,6 @@ def convert_dict_to_2d_array(d):
     return arr
 
 
-def make_matrix_image(matrix, output_path, xlabels=[], ylabels=[], title="", cmap=None):
-    if not xlabels:
-        xlabels = range(len(matrix))
-    # we assume it is a square matrix
-    if not ylabels:
-        ylabels = range(len(matrix[0]))
-    fig, ax = plt.subplots()
-    im = ax.imshow(matrix, cmap, vmin=0, vmax=1)
-    ax.set_xlabel("Target tree")
-    ax.set_ylabel("Source tree")
-    ax.set_xticks(range(len(xlabels)), labels=xlabels)
-    ax.set_yticks(range(len(ylabels)), labels=ylabels)
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            text = ax.text(
-                j, i, round(matrix[i][j], 3), ha="center", va="center", color="w"
-            )
-    ax.set_title(title)
-    fig.set_size_inches(len(matrix), len(matrix[0]))
-    fig.tight_layout()
-    plt.savefig(output_path, dpi=100)
 
 
 def check_input_trees(tlist: list[ete4.Tree]) -> bool:
